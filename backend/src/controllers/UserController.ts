ee3ecb6e615b135acca4f25a57247c8b70256376
AntonioWingert <antoniobwingert@gmail.com>
2023-09-14 19:59:36 -0300

    finish features

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -74,10 +74,6 @@ export const update = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  if (req.user.profile !== "admin") {
-    throw new AppError("ERR_NO_PERMISSION", 403);
-  }
-
   const { userId } = req.params;
   const userData = req.body;
   const profileImage = req.file ? req.file.filename : null;
@@ -119,12 +115,6 @@ export const updateStatus = async (
   res: Response
 ): Promise<Response> => {
   const { userId, alteredUserId, lastStatusId, actualStatusId } = req.body;
-
-  if (userId !== alteredUserId) {
-    if (req.user.profile !== "admin") {
-      throw new AppError("ERR_NO_PERMISSION", 403);
-    }
-  }
 
   await UpdateStatusService({
     userId,
